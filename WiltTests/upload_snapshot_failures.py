8995882675bdcbce087ea6fe73ca77cf466197d5
Oliver <oliveroneill04@gmail.com>
2020-04-05 22:15:25 +1000

    Add snapshot upload to S3 to debug failures on CI

---
+++ WiltTests/upload_snapshot_failures.py
@@ -0,0 +1,17 @@
+import os
+import time
+import shutil
+import tinys3
+
+if __name__ == '__main__':
+    filename = 'failures_{}'.format(time.time())
+    failureDir = 'FailureDiffs/'
+    if os.path.isdir(failureDir):
+        shutil.make_archive(filename, 'zip', failureDir)
+        filename += '.zip'
+
+        conn = tinys3.Connection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'], tls=True)
+        with open(filename, 'rb') as f:
+            conn.upload(filename, f, os.environ['UPLOAD_IOS_SNAPSHOT_BUCKET_NAME'])
+    else:
+        print("No failures found")
