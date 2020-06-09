e813cfdcfadd402261061c0c7f8611e3ea569f76
Oliver <oliveroneill04@gmail.com>
2020-06-09 20:37:21 +1000

    Fix snapshot failure upload script
    Seems my access key stopped working

--- WiltTests/upload_snapshot_failures.py
+++ WiltTests/upload_snapshot_failures.py
@@ -1,6 +1,7 @@
 import os
-import time
 import shutil
+import time
+
 import tinys3
 
 if __name__ == '__main__':
@@ -10,8 +11,13 @@ if __name__ == '__main__':
         shutil.make_archive(filename, 'zip', failureDir)
         filename += '.zip'
 
-        conn = tinys3.Connection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'], tls=True)
-        with open(filename, 'rb') as f:
-            conn.upload(filename, f, os.environ['UPLOAD_IOS_SNAPSHOT_BUCKET_NAME'])
+        conn = tinys3.Connection(
+            os.environ['AWS_ACCESS_KEY_ID'],
+            os.environ['AWS_SECRET_ACCESS_KEY'],
+            tls=True,
+            endpoint='s3-us-west-2.amazonaws.com'
+        )
+        f = open(filename, 'rb')
+        print(conn.upload(filename, f, os.environ['UPLOAD_IOS_SNAPSHOT_BUCKET_NAME']))
     else:
         print("No failures found")
